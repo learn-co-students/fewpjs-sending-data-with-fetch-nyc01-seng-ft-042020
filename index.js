ccb87f6bf185af6fa0d8fb482dbed1878671329b
rwilt <rosie.wilt@gmail.com>
2020-06-06 22:09:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+const setUrl = "http://localhost:3000/users"
+function submitData(userName,userEmail){
+let formData = {
+    name: userName,
+    email: userEmail
+};
+let postObj = {
+    method: "POST",
+    headers: {
+        "Content-Type":"application/json", 
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+};
+    fetch(setUrl, postObj)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+       let objId = object.id
+       let element = document.createElement('p');
+       let body = document.getElementsByTagName('body')[0];
+       body.append(objId);
+       
+    })
+    .catch(function(error){
+        alert("Error!");
+        console.log(error.message);
+    });
+    return postObj
+}
